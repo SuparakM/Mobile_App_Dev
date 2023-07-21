d2f9c2369dbee82d6de69f9eea8513681a47ffff
SuparakM <suparak2408@gmail.com>
2023-07-21 14:24:52 +0700

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:funny_quotes_homework/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FunnyQuotesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
